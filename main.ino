7010fc960d31f155c99c9a7efa4ca5392ffc141e
Used-Computer <lgwilson2@students.wcpss.net>
2025-09-26 14:45:53 -0400

    Update main.ino

--- main.ino
+++ main.ino
@@ -1,3 +1,39 @@
 #include <PestoLink-Receive.h>
 #include <Alfredo_NoU3.h>
 #include <constants.h>
+
+// Motors.
+
+NoU_Motor frontLeftMotor(1);
+NoU_Motor frontRightMotor(2);
+NoU_Motor backLeftMotor(3);
+NoU_Motor backRightMotor(4);
+NoU_Motor intakeMotor(5);
+NoU_Motor elevator(6);
+
+// Drivetrain.
+
+void setup() {
+  PestoLink.begin("Robot Name");
+  Serial.begin(115200);
+
+  NoU3.begin();
+  NoU3.setServiceLight(LIGHT_ENABLED);
+
+  frontLeftMotor.setBrakeMode(true);
+  frontRightMotor.setBrakeMode(true);
+  backLeftMotor.setBrakeMode(true);
+  backRightMotor.setBrakeMode(true);
+}
+
+void loop() {
+  chassis(); 
+  arm();
+  claw();
+  intake();
+
+  intakeMotor.set(intakeT);
+  stageI.write(angleI);
+  stageII.write(angleII);
+  clawServo.write(clawAngle);
+}
